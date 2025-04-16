ec84fbe187c17654a0285d519e7503bfe9ea1691
akmal81 <sah64078@gmail.com>
2025-04-16 10:15:47 +0600

    if else

---
+++ function.js
@@ -0,0 +1,61 @@
+// const invitation = !false;
+// const checkPocket = 1200;
+
+// if (invitation && checkPocket >= 1000){
+//     console.log('go to the party and take a gift')
+// }
+// else if (!invitation)   {
+//     console.log('Kick out form friendlist')
+    
+// }
+// else{
+//     console.log('go to the party')
+// }
+
+
+// const tea = true;
+// const biscut = !true;
+
+// if (tea) {
+//     console.log('Would you like biscut');
+//     if (biscut) {
+//         console.log('tea and biscut')
+//     }
+//     else{
+//         console.log('tea is ready')
+//     }
+
+  
+// }
+// else{
+//     console.log('star jalsha')
+// }
+
+// const isActive = !true;
+// const subscribtionPremium = !true;
+
+// if (isActive) {
+//     if (subscribtionPremium) {
+//         console.log('Premium')
+//     }
+//     else{
+//         console.log('free account')
+//     }
+// }
+// else{
+//     console.log('Create a account')
+// }
+
+// const checkFreez = !true;
+// const foodDeliveryApp= !true;
+// if (checkFreez) {
+//     console.log('head the food')
+// }
+// else if (!checkFreez) {
+//     if (foodDeliveryApp) {
+//         console.log('Order Food')
+//     }
+//     else{
+//         console.log('I am roza')
+//     }
+// }
--- key.js
+++ key.js
@@ -15,43 +15,83 @@
 
 // prb 2
 
-const age =61;
-const price = 500;
-if (age <= 12){
-    console.log('No charge')
-}
-else if (age>= 60) {
-    const discount = price /100 * 50;
-    const payable = price-discount;
-    console.log("over 50 age will get 50% discount price", payable)
-} else {
-    console.log('no discount')
-}
+// const age =61;
+// const price = 500;
+// if (age <= 12){
+//     console.log('No charge')
+// }
+// else if (age>= 60) {
+//     const discount = price /100 * 50;
+//     const payable = price-discount;
+//     console.log("over 50 age will get 50% discount price", payable)
+// } else {
+//     console.log('no discount')
+// }
 
-// prb 3
+// // prb 3
 
-const bankBalance =4000;
+// const bankBalance =4000;
 
-if (bankBalance<= 1000) {
-    console.log('diposit first')
-} else if(bankBalance>=1000 && bankBalance<=5000){
-    console.log('Bindas enjoy')   
-}
-else{
-    console.log('Marry me')
-}
+// if (bankBalance<= 1000) {
+//     console.log('diposit first')
+// } else if(bankBalance>=1000 && bankBalance<=5000){
+//     console.log('Bindas enjoy')   
+// }
+// else{
+//     console.log('Marry me')
+// }
 
-// prb 4
-const marks = 40;
+// // prb 4
+// const marks = 40;
 
-if (marks < 50) {
-    console.log('You are fail')
-}
- else if(marks >=50 && marks<= 80){
-    console.log("you are pass")
-}
-else{
-    console.log('A+')
-}
+// if (marks < 50) {
+//     console.log('You are fail')
+// }
+//  else if(marks >=50 && marks<= 80){
+//     console.log("you are pass")
+// }
+// else{
+//     console.log('A+')
+// }
+
 // prb 5
+const pageSize = 100;
 
+if (pageSize<=100){
+    console.log('small Book');
+}
+else if (pageSize>=100 && pageSize<=500) {
+    console.log('Midsize book')
+}
+else{
+    console.log('Heart attact book')
+}
+
+// prb 6
+
+const tempareture = 0;
+
+if (tempareture<=0) {
+    console.log('ice')
+}
+
+else if (tempareture> 0 && tempareture<=20) {
+    console.log('cool cool')
+}
+else{
+    console.log('Hot')
+}
+// 7
+
+const level = 10
+if (level<=10) {
+    console.log('novice')
+    
+}
+else if (level >10 && level <=50) {
+    console.log('Expen')
+    
+}
+else{
+    console.log('Pro Gammer')
+}
